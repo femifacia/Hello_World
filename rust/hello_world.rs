9b44dab831b7f78e67093e23e207ce488935c833
femifacia <femi.facia@epitech.eu>
2022-06-16 17:08:38 +0100

    [ADD] pascal and [UPD] images directory

--- rust/hello_world.rs
+++ rust/hello_world.rs
@@ -1,6 +1,17 @@
-fn main() {
+/*fn main() {
     // Statements here are executed when the compiled binary is called
 
     // Print text to the console
     println!("Hello World!");
+}*/
+
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+
+    handle.write_all(b"Hello World")?;
+
+    Ok(())
 }
